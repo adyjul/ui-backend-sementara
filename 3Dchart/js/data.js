daa02714c85f9583331d59b89b08bd39a81694b5
adyjul <adyjuliyanto2001@gmail.com>
2021-10-02 14:38:22 +0700

    add 3d chart

---
+++ 3Dchart/js/data.js
@@ -0,0 +1,23 @@
+data = [
+    {
+      barang: "lemari",
+      jumlah: 10
+    },
+    {
+      country: "AC",
+      jumlah: 30
+    },
+    {
+      barang: "Kursi",
+      jumlah: 40
+    },
+    {
+      barang: "Komputer",
+      jumlah: 16
+    },
+    {
+      barang: "Meja",
+      jumlah: 10
+    },         
+  ];
+ 
